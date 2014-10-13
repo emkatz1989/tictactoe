7a4dbe3e6ef62c2fa8817fdbe228e046a119590b
Eric Katz <emkatz1989@gmail.com>
2014-10-13 15:04:02 -0700

    update with game logic for winning

--- main.js
+++ main.js
@@ -1,77 +1,68 @@
-var appTicTac=angular.module('TicTac',[]);
+var appTicTac=angular.module('appTicTac',[]);
 
 appTicTac.controller('myController', function($scope){
 	console.log("here");
 
 	$scope.cellList=[
-	{status: "c0"},
-	{status: "c1"},
-	{status: "c2"},
-	{status: "c3"},
-	{status: "c4"},		
-	{status: "c5"},
-	{status: "c6"},
-	{status: "c7"},
-	{status: "c8"}
+	{name: "c0", status: "true"},
+	{name: "c1", status: "true"},
+	{name: "c2", status: "true"},
+	{name: "c3", status: "true"},
+	{name: "c4", status: "true"},		
+	{name: "c5", status: "true"},
+	{name: "c6", status: "true"},
+	{name: "c7", status: "true"},
+	{name: "c8", status: "true"}
 	];
 
-	$scope.movecounter = 0 ;
 
-//   // $scope.testJS = function() {
-//   //   console.log('Correctly accessing JS function.') ;
-//   // } ;
+ $scope.moveCounter = 0;
   
-  $scope.playerPicks = function(thisCell) {
-    $scope.movecounter = $scope.movecounter + 1 ;
+ $scope.playerPicks = function(cell) {
 
+ if(cell.status=="true"){
 
-    if(thisCell.status=='X' || thisCell.status=='O'){
-    	return;
+	cell.status="false";
+
+    $scope.moveCounter=$scope.moveCounter + 1;
+
+    if (($scope.moveCounter % 2) == 1) {
+      cell.name = "X" ; } 
+    else {
+      cell.name = "O" ;
+
     };
 
-    console.log("Cell was: " + thisCell.status) ;
-    if (($scope.movecounter % 2) == 1) {
-      thisCell.status = "X" ;  
-    } else {
-      thisCell.status = "O" ;
-    } ;
-  };
+  if(
+//horizontal//
+$scope.cellList[0].status===$scope.cellList[1].status && $scope.celllist[0].status==$scope.cellList[2].status ||
+$scope.cellList[3].status===$scope.cellList[4].status && $scope.cellList[3].status===$scope.cellList[5].status ||
+$scope.cellList[6].status===$scope.cellList[7].status && $scope.cellList[6].status===$scope.cellList[8].status||
+
+//vertical//
+$scope.cellList[0].status===$scope.cellList[3].status && $scope.cellList[0].status==$scope.cellList[6].status ||
+$scope.cellList[1].status===$scope.cellList[4].status && $scope.cellList[1].status==$scope.cellList[7].status ||
+$scope.cellList[2].status===$scope.cellList[5].status && $scope.cellList[2].status==$scope.cellList[8].status ||
+
+
+//diagonal//
+$scope.cellList[0].status===$scope.cellList[4].status && $scope.cellList[0].status==$scope.cellList[8].status || 
+$scope.cellList[2].status===$scope.cellList[4].status && $scope.cellList[2].status==$scope.cellList[6].status){
+  console.log("'$scope.cellList.status' + 'wins!'");
+
+}
+
+else{
+	return;
+}
+
+    };
+
+    console.log("Cell was: " + cell.name);
+   
+    	
+}
 });
    
 
 
-
-
-
-
-
-
-
-
-//   //   console.log("Cell is now: " + thisCell.status) ;
-//   // }; 
-
-// };
-
-// }
-	// $scope.checkWinner=function(){
-	// 	thisCell.true=[[c0,c4,c8],[] ]
-	// }
-
-
-
-
-
-// 		$scope.movecounter=0;
-		
-// 		$scope.playerSelect=function(thisCell){
-// 		$scope.movecounter = $scope.movecounter + 1;
-// 		console.log("Cell was: " + thisCell.status);
-// 		if(($scope.movecounter % 2)==1){
-// 			thisCell.status="X" ;
-// 			}else{
-// 				thisCell.status="O";
-// 			};
-
-// 			}
-// });
